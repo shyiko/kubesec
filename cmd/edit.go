37b0d07fd7adbd989a067c1bb87ec84dacd55b98
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-08-08 17:31:35 -0700

    Added support for $EDITOR with args

--- cmd/edit.go
+++ cmd/edit.go
@@ -3,10 +3,12 @@ package cmd
 import (
 	"bytes"
 	"errors"
+	log "github.com/Sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
+	"unicode"
 )
 
 func Edit(content []byte) ([]byte, error) {
@@ -34,18 +36,59 @@ func Edit(content []byte) ([]byte, error) {
 func openInEditor(path string) error {
 	editor := os.Getenv("EDITOR")
 	var cmd *exec.Cmd
+	var args []string
 	if editor == "" {
-		cmd = exec.Command("which", "vim") // todo: vim -n -i NONE -u NONE
+		cmd = exec.Command("which", "vim")
 		out, err := cmd.Output()
 		if err != nil {
 			return errors.New("$EDITOR not defined")
 		}
-		cmd = exec.Command(strings.Split(string(out), "\n")[0], path)
+		editor, args = strings.Split(string(out), "\n")[0], []string{"-n", "-i", "NONE", "-u", "NONE"}
 	} else {
-		cmd = exec.Command(editor, path)
+		editorCmd := parseCommand(editor)
+		editor, args = editorCmd[0], editorCmd[1:]
 	}
+	args = append(args, path)
+	log.Debugf(`Executing %s %s`, editor, strings.Join(args, " "))
+	cmd = exec.Command(editor, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
+}
+
+func parseCommand(command string) []string {
+	var chunks []string
+	i, j := 0, 0
+	var r rune
+	lastQuote := rune(0)
+	for j, r = range command {
+		switch {
+		case r == lastQuote && command[j-1] != '\\':
+			lastQuote = rune(0)
+		case lastQuote != rune(0):
+			break
+		case unicode.In(r, unicode.Quotation_Mark):
+			lastQuote = r
+		default:
+			if unicode.IsSpace(r) {
+				chunks = append(chunks, unquote(command[i:j]))
+				i = j + 1
+			}
+		}
+	}
+	if i <= j {
+		chunks = append(chunks, unquote(command[i:j+1]))
+	}
+	return chunks
+}
+
+func unquote(value string) string {
+	runes := []rune(value)
+	if len(runes) >= 2 {
+		if unicode.In(runes[0], unicode.Quotation_Mark) && runes[0] == runes[len(runes)-1] {
+			value = string(runes[1 : len(runes)-1])
+		}
+	}
+	return strings.Replace(strings.Replace(value, `\"`, `"`, -1), `\'`, `'`, -1)
 }
--- cmd/edit_test.go
+++ cmd/edit_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -16,5 +17,13 @@ func TestEncryptDecrypt(t *testing.T) {
 	}
 	if string(actual) != expected {
 		t.Fatalf("actual: %#v != expected: %#v", string(actual), expected)
+	}
+}
+
+func TestParseCommand(t *testing.T) {
+	actual := parseCommand(`path/to/bin arg1 "arg2" 'arg3' "'" '"' "" '' "a g" 'a g' "\"" '\''`)
+	expected := []string{"path/to/bin", "arg1", "arg2", "arg3", "'", "\"", "", "", "a g", "a g", "\"", "'"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("actual: %#v != expected: %#v", actual, expected)
 	}
 }
